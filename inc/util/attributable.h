df5869935ea1a0c9f368527730b9cd4d3a5eba5b
manuelthieme <info@manuel-thieme.de>
2019-09-02 11:00:15 +0200

    added documentation for attributable tree

--- inc/util/attributable.h
+++ inc/util/attributable.h
@@ -3,10 +3,28 @@
 #include <vector>
 #include <string>
 
+
+/**
+ * An Object that holds one or more attributes to be identified
+ */
 class Attributable {
-    std::vector<std::string> attributes;
+    std::vector<std::string> attributes; /**< List of attributes */
 
 public:
+    /**
+     * add an attribute to the object
+     * @param attribute
+     *   attribute to add
+     */
     void add_attribute(std::string attribute);
+
+    /**
+     * check if the Object holds a given attribute
+     *
+     * @param attribute
+     *   attribute to check for
+     * @returns
+     *   True if attribute is in the list of given attributes for this object. False otherwise.
+     */
     bool has_attribute(std::string attribute);
 };
--- inc/util/attribute_tree.h
+++ inc/util/attribute_tree.h
@@ -7,9 +7,22 @@
 #include <util/tree.h>
 
 
+/**
+ * Node of an AttributeTree
+ *
+ * This holds the recursive algorithms to find Nodes based on Attributes
+ */
 template<class T>
 class TreeNode<T, typename std::enable_if<std::is_base_of<Attributable, T>::value>::type> : TreeNodeBase<T> {
 public:
+    /**
+     * Find first Child that holds a given attribute
+     *
+     * @param attribute
+     *   attribute to search for
+     * @returns
+     *   first found Object that holds the given attribute. nullptr if no such object exists.
+     */
     T *find_first_with_attribute(std::string attribute) const {
         if (this->_node->has_attribute(attribute)) {
             return this->_node;
@@ -23,6 +36,14 @@ public:
         return nullptr;
     }
 
+    /**
+     * Find all Childs that hold a given attribute
+     *
+     * @param attribute
+     *   attribute to search for
+     * @returns
+     *   List of Objects that hold the given attribute.
+     */
     std::vector<T *> find_all_with_attribute(std::string attribute) const {
         std::vector<T *> vec;
         if (this->_node->has_attribute(attribute)) {
@@ -36,9 +57,22 @@ public:
     }
 };
 
+/**
+ * A Tree that holds Attributable Objects.
+ *
+ * It is possible to find one or more objects in this tree based on the given attributes
+ */
 template<class T>
 class Tree<T, typename std::enable_if<std::is_base_of<Attributable, T>::value>::type> : TreeBase<T> {
 public:
+    /**
+     * Find first Object in tree that holds a given attribute
+     *
+     * @param attribute
+     *   Attribute to search for
+     * @returns
+     *   First found Object that holds the given attribute. nullptr if no such object exists.
+     */
     T *find_first_with_attribute(std::string attribute) const {
         if (this->root == nullptr) {
             return nullptr;
@@ -46,6 +80,15 @@ public:
         return this->root->find_first_with_attribute(attribute);
     }
 
+
+    /**
+     * Find all Objects in tree that hold a given attribute
+     *
+     * @param attribute
+     *   attribute to search for
+     * @returns
+     *   List of Objects that hold the given attribute.
+     */
     std::vector<T *> find_all_with_attribute(std::string attribute) const {
         if (this->root == nullptr) {
             return std::vector<T *>();
